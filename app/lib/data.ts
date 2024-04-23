94260085b290bdf1c856bc110db6ede16966f4b7
Anton S <shapikcool@gmail.com>
2024-04-23 13:34:43 +0700

    data mutation and error handling;

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,11 +21,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -38,7 +38,7 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
     console.log('Latest Invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -52,7 +52,7 @@ export async function fetchLatestInvoices() {
       amount: formatCurrency(invoice.amount),
     }));
 
-    console.log('Latest Invoices fetched with 2s delay.');
+    // console.log('Latest Invoices fetched with 2s delay.');
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -173,7 +173,6 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
